9433f6f1adb9529a5413d38a0c49d2caa911ed83
Ewerton Franco <ewefsilva@gmail.com>
2024-09-18 16:10:45 -0300

    Refactor Home component and add WorkoutView
    Moved workout creation form and logic from Home component to a new WorkoutView component. Updated the routing in App.tsx to include the new route for the WorkoutView.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/Home.tsx";
+import WorkoutView from "./routes/Workout.view.tsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/workout" element={<WorkoutView />} />
     </Routes>
   );
 }
